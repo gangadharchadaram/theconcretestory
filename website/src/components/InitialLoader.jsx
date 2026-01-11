0e794a2f9e2865b0429cdc126d8e32a90c9a1dbf
gangadharchadaram <saigangadhar06@gmail.com>
2026-01-11 09:25:41 +0530

    removed logo

--- website/src/components/InitialLoader.jsx
+++ website/src/components/InitialLoader.jsx
@@ -2,7 +2,7 @@ const InitialLoader = () => {
   return (
     <div className="fixed inset-0 bg-[#c89b3c] z-[9999] flex items-center justify-center">
       <div className="flex items-center gap-4">
-        <img src="/logo_tcs.png" alt="Logo" className="h-40" />
+        {/* <img src="/logo_tcs.png" alt="Logo" className="h-40" /> */}
         <h1>THE CONCRETE STORY</h1>
         <div className="w-10 h-10 border-4 border-black border-t-transparent rounded-full animate-spin"></div>
       </div>
